642fb528fba02090c13bda6464535b888a83a3a3
derekmurawsky <dmurawsky@gmail.com>
2013-06-27 10:20:16 -0400

    Initial seed of files

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'Gpg4win' 
+$installerType = 'EXE' 
+$url = 'http://files.gpg4win.org/gpg4win-2.1.1.exe' 
+$url64 = $url 
+$silentArgs = '/S' 
+$validExitCodes = @(0) 
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
